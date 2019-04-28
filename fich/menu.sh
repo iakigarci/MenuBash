3e4b9359cc8b3c508eef36d54bc254a150ba020f
iakigarci <garcianoya@hotmail.es>
2019-04-28 18:53:12 +0200

    Arregladom el test de php

--- fich/menu.sh
+++ fich/menu.sh
@@ -2,21 +2,38 @@
 #Importar funciones de otros ficheros
 
 ###########################################################
-#                  1) INSTALL APACHE                     #
+#                 SUBPROGRAMAS ADICIONALES                #
+###########################################################
+
+function checkApacheStatus()
+{
+    aux=$(aptitude show apache2 | grep "State: installed")
+	aux2=$(aptitude show apache2 | grep "Estado: instalado")
+	aux3=$aux$aux2
+	aux4=0
+	if [ -z "$aux3" ]
+	then #el if devuelve 0 si es true y 1 si es false
+        let aux4=0
+    else
+        let aux4=1
+    fi
+    return $aux4
+}
+
+###########################################################
+#                  1) INSTALL APACHE                      #
 ###########################################################
 function apacheInstall()
 {
-	aux=$(aptitude show apache2 | grep "State: installed")
-	aux2=$(aptitude show apache2 | grep "Estado: instalado")
-	aux3=$aux$aux2
-	if [ -z "$aux3" ]
-	then 
- 	  echo "instalando ..."
- 	  sudo apt-get install apache2
-	else
-   	  echo "apache ya estaba instalado"
+	echo "instalando ..."
+    sudo apt-get --assume-yes install apache2
+    echo "realizando prueba de conexión..."
+    aux=$(sudo netstat -anp | grep apache)
+    if [[ -z aux ]]
+    then 
+        echo "¡PROBLEMAS CON LA CONEXIÓN"
+        
     
-	fi 
 }
 
 ###########################################################
@@ -24,62 +41,45 @@ function apacheInstall()
 ###########################################################
 function apacheAction()
 {
-
+    echo "activado apache..."
+    sudo service apache2 start
+    aux=$( sudo service apache2 status | grep "● apache2.service - The Apache HTTP Server")
+    echo $aux
 }
+
 ###########################################################
 #                  3) INSTALAR PHP                        #
 ###########################################################
 function phpInstall()
 {
+    aux=$(aptitude show php | grep "State: installed")
+	aux2=$(aptitude show php | grep "Estado: instalado")
+	aux3=$aux$aux2
+	aux4=0
+	if [ -z "$aux3" ]
+	then
+        echo "instalando..."
+        sudo apt-get --assume-yes install php
+        sudo apt install php libapache2-mod-php
+    else
+        echo "php ya estaba instalado"
+    fi
+    echo "reiniciando apache" 
+    sudo service apache2 restart
 }
+
 ###########################################################
 #                  4) TESTEAR PHP                         #
 ###########################################################
 function phpTest()
 {
+    echo "creando el archivo test.php"
+    sudo sh -c 'echo "<?php phpinfo(); ?>" ?> /var/www/html/test.php'
+    firefox-esr http://localhost/test.php
+    
+    #FALTA COMPROBAR PERMISOS
 }
-###########################################################
-#                  5) CREAR PYTHON3                       #
-###########################################################
-function createPython()
-{
-}
-###########################################################
-#                  6) INSTALAR PAQUETES                   #
-###########################################################
-function installPackages()
-{
-}
-###########################################################
-#                  7) TESTEAR PY VM                       #
-###########################################################
-function testPython()
-{
-}
-###########################################################
-#                  8) PROBAR COMPLEJIDAD                  #
-###########################################################
-function testComplejidad()
-{
-}
-###########################################################
-#                  9) VISUALIZAR COMPLEJIDAD              #
-###########################################################
-function viewContent()
-{
-}
-###########################################################
-#                  10) LOGS Y ERRORES                     #
-###########################################################
-function logsErrors()
-{
-}
-###########################################################
-#                  11) CONTROLA SSH                       #
-###########################################################
-function controlSSH()
-{
-}
+
 ###########################################################
 #                     12) SALIR                          #
 ###########################################################
@@ -99,16 +99,35 @@ opcionmenuppal=0
 while test $opcionmenuppal -ne 12
 do
 	#Muestra el menu
+	echo -e "\n \n"
 	echo -e "1 Instala Apache \n"
+	echo -e "2 Activar Apache \n"
 	echo -e "12 Salir \n"
-	read -p "Elige una opcion:" opcionmenuppaltraductor
+	echo -e "\n \n"
+	read -p "Elige una opcion:" opcionmenuppal
 	case $opcionmenuppal in
-			1) apacheInstall;;
-			2) apacheAction;;
-			3) 
-			12) fin;;
-			*) ;;
-
+			1) 
+                if checkApacheStatus
+                then 
+                    apacheInstall
+                else
+                    echo "apache ya estaba instalado"
+                fi
+                ;;
+            2)
+                if checkApacheStatus
+                then 
+                    apacheInstall
+                else
+                    apacheAction
+                fi 
+                ;;
+            3)
+                
+			12) 
+                fin;;
+			*) 
+                ;;
 	esac 
 done 
 

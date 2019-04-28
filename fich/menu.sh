ab6453416fdec9fdaff2fb63db40b0eaf543ff7e
iakigarci <garcianoya@hotmail.es>
2019-04-28 21:31:34 +0200

    Acabando el punto 5

--- fich/menu.sh
+++ fich/menu.sh
@@ -5,13 +5,14 @@
 #                 SUBPROGRAMAS ADICIONALES                #
 ###########################################################
 
-function checkApacheStatus()
+function checkStatus()
 {
-    aux=$(aptitude show apache2 | grep "State: installed")
-	aux2=$(aptitude show apache2 | grep "Estado: instalado")
+    echo -e "Comprobando el estado de $1 \n"
+    aux=$(aptitude show $1 | grep "State: installed")
+	aux2=$(aptitude show $1 | grep "Estado: instalado")
 	aux3=$aux$aux2
 	aux4=0
-	if [ -z "$aux3" ]
+	if [[ -z "$aux3" ]]
 	then #el if devuelve 0 si es true y 1 si es false
         let aux4=0
     else
@@ -19,6 +20,7 @@ function checkApacheStatus()
     fi
     return $aux4
 }
+
 
 ###########################################################
 #                  1) INSTALL APACHE                      #
@@ -32,8 +34,7 @@ function apacheInstall()
     if [[ -z aux ]]
     then 
         echo "¡PROBLEMAS CON LA CONEXIÓN"
-        
-    
+    fi
 }
 
 ###########################################################
@@ -52,18 +53,9 @@ function apacheAction()
 ###########################################################
 function phpInstall()
 {
-    aux=$(aptitude show php | grep "State: installed")
-	aux2=$(aptitude show php | grep "Estado: instalado")
-	aux3=$aux$aux2
-	aux4=0
-	if [ -z "$aux3" ]
-	then
-        echo "instalando..."
-        sudo apt-get --assume-yes install php
-        sudo apt install php libapache2-mod-php
-    else
-        echo "php ya estaba instalado"
-    fi
+    echo "instalando..."
+    sudo apt-get --assume-yes install php
+    sudo apt install php libapache2-mod-php
     echo "reiniciando apache" 
     sudo service apache2 restart
 }
@@ -81,6 +73,40 @@ function phpTest()
 }
 
 ###########################################################
+#                  5) CREAR PYTHON3                       #
+###########################################################
+function createPython()
+{
+    dir=$(pwd | grep fich)
+    if [ -n dir ]
+    then 
+        sudo apt-get --assume-yes install python-virtualenv virtualenv
+        if checkStatus python3 
+        then
+            echo -e "instalando python3... \n"
+            sudo apt-get --asume-yes install python3
+        fi
+    fi
+    user=$(whoami)
+    root="root"
+    if [ "$user" == "$root" ]
+    then
+        echo "Como eres usuario root, se hace un log out para volver a ser un usuario"
+        exit
+        user=$(whoami)
+    fi
+    echo -e " \n"
+    #Creamos entorno virtual 
+    virtualenv env --python=python3
+    dir="/home/$user/env"
+    
+    #Activamos entorno virtual
+    echo "Se va ha activar el entorno virtual en $dir"
+    cd $dir
+    source bin/activate
+}   
+
+###########################################################
 #                     12) SALIR                          #
 ###########################################################
 
@@ -91,7 +117,7 @@ function fin()
 	if [ $respuesta == "N" ] 
 		then
 			opcionmenuppal=0
-		fi	
+    fi	
 }
 
 ### MAIN ###
@@ -102,12 +128,21 @@ do
 	echo -e "\n \n"
 	echo -e "1 Instala Apache \n"
 	echo -e "2 Activar Apache \n"
+	echo -e "3 Instalar el modulo php \n"
+	echo -e "4 Testear PHP \n"
+	echo -e "5 Crear un entorno para Python3 \n"
+	echo -e "6 Instalar los paquetes para la aplicación \n"
+	echo -e "7 Probar programa.py \n"
+	echo -e "8 Instalar la aplicación \n"
+	echo -e "9 Visualizar la aplicación \n"
+	echo -e "10 Visualizar logs y errores de apache \n"
+	echo -e "11 Controlar los intentos de conexióm de Apache \n"
 	echo -e "12 Salir \n"
 	echo -e "\n \n"
 	read -p "Elige una opcion:" opcionmenuppal
 	case $opcionmenuppal in
 			1) 
-                if checkApacheStatus
+                if checkStatus apache2
                 then 
                     apacheInstall
                 else
@@ -115,7 +150,7 @@ do
                 fi
                 ;;
             2)
-                if checkApacheStatus
+                if checkStatus apache2
                 then 
                     apacheInstall
                 else
@@ -123,7 +158,23 @@ do
                 fi 
                 ;;
             3)
-                
+                if checkStatus php
+                then 
+                    phpInstall
+                else
+                    echo "php ya estaba instalado"
+                fi
+                ;;
+            4)
+                if checkStatus php
+                then
+                    phpInstall
+                else
+                    phpTest
+                fi
+                ;;
+            5)
+                createPython;;
 			12) 
                 fin;;
 			*) 
